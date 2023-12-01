6a0efeba27aaa7eb80d182539b2d7e0b11c25868
KaterynaValuiska <katya.barry.ubf@gmail.com>
2023-12-01 22:51:56 +0100

    45

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/test-task-rental-cars">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
