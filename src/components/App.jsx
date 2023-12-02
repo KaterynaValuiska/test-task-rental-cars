1690d5190b769d1b7877491e9aa5405dfb184b6d
KaterynaValuiska <katya.barry.ubf@gmail.com>
2023-12-02 11:25:08 +0100

    fix catalog

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import Layout from './Layout';
 import Home from './pages/Home';
+import Catalog from './pages/Catalog';
 
 export const App = () => {
   return (
@@ -8,7 +9,7 @@ export const App = () => {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="/catalog" element={<div>catalog</div>} />
+          <Route path="/catalog" element={<Catalog />} />
           <Route path="/favorites" element={<div>favorite</div>} />
           <Route path="*" element={<Home />} />
         </Route>
