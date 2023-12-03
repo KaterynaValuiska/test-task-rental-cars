610c608c6a80540a296741c7eb54b9cf1355d0f3
KaterynaValuiska <katya.barry.ubf@gmail.com>
2023-12-03 11:34:31 +0100

    fix navigation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,8 +10,8 @@ export const App = () => {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="/catalog" element={<Catalog />} />
-          <Route path="/favorites" element={<Favorites />} />
+          <Route path="catalog" element={<Catalog />} />
+          <Route path="favorites" element={<Favorites />} />
           <Route path="*" element={<Home />} />
         </Route>
       </Routes>
